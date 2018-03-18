129939e76743ebb830f4b19f7f11ed157e749f94
SHIVANI264 <30775079+SHIVANI264@users.noreply.github.com>
2018-03-18 21:53:07 +0530

    Update Shipment.java

--- src/main/java/com/monsanto/mbt/Shipment.java
+++ src/main/java/com/monsanto/mbt/Shipment.java
@@ -1,9 +1,16 @@
+
+
 package com.monsanto.mbt;
 
 public class Shipment {
 
+List<Widget> l1 = new ArrayList<Widget>() ;
+  
+  for (int i=0 ,i <10 ; i++)
+  {
+    Widget w1 = new Widget(serialnumber,color, date)
+    l1.add(w1);
+ }
 
-
-
-
-}
+  Collections.sort(l1);
+  
