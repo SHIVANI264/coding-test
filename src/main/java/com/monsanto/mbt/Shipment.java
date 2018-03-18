a43ccf161a4b444d9596e97bc5d5bed79498f42f
SHIVANI264 <30775079+SHIVANI264@users.noreply.github.com>
2018-03-19 00:44:58 +0530

    Update Shipment.java

--- src/main/java/com/monsanto/mbt/Shipment.java
+++ src/main/java/com/monsanto/mbt/Shipment.java
@@ -5,18 +5,33 @@ package com.monsanto.mbt;
 public class Shipment {
 
 
-  public void sorted_by_date()
+  public void sorted_by_color()
   {
     List <Widgets> l1 = new ArrayList<Widget>();
     
-    Collections.sort(l1, new Comparator<Widget>() {
-		@Override
-		public int compare(Widget o1, Widget o2) {
-			return o1.getColor() - o2.getColor();
-		}
+    Collections.sort(l1, new Comparator<Widget>() )
+   {
+    @Override
+   public int compare(Widget o1, Widget o2) 
+   {
+   return o1.getColor() - o2.getColor();
+   }
       
-    }
-                     }
+    
+public void sort_ByDATE()
+{
+List <Widgets> l1 = new ArrayList<Widget>();
+	
+Collections.sort(l1, new Comparator<Developer>())
+{
+@Override
+public int compare(Widget w1, Widget w2) 
+{
+return o1.getDate() - o2.getDate();
+}
+}
+}	
+      
 
     
   
